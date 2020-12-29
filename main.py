14edfb36d464d093c1915c82ee44b32a7bd30e92
Ilia Kamyshev <ikdesign2015@yandex.ru>
2020-12-30 00:03:38 +0300

    menu updated

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/hello/<name>")
+def ind(name):
+    return "Hello, " + name
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
